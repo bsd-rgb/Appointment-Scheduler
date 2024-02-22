6c65d0ed2c79bcacf519634455d95568396fa364
Brandi <brandi.davis2483@gmail.com>
2024-02-21 23:32:05 -0600

    Added error handling to the login screen controller.

--- src/main/java/controller/LoginScreenController.java
+++ src/main/java/controller/LoginScreenController.java
@@ -4,8 +4,7 @@ import dao.UsersDao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import model.Users;
 
 import java.net.URL;
@@ -16,50 +15,70 @@ public class LoginScreenController implements Initializable {
 
     @FXML
     private TextField passwordTxtField;
-
     @FXML
     private Label locationLbl;
-
     @FXML
     private TextField usernameTxtField;
-
     private boolean french = false;
+    Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION);
+    Alert errorAlert = new Alert(Alert.AlertType.ERROR);
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         ZoneId zoneId = ZoneId.systemDefault();
-        locationLbl.setText(zoneId.toString());
+        locationLbl.setText("Location: " + zoneId.toString());
 
     }
 
     @FXML
     void onActionLogin(ActionEvent event) {
 
-        //when the button is selected, we want to check the username and password
-
         try {
-
             String userName = usernameTxtField.getText();
             String password = passwordTxtField.getText();
-
             if(UsersDao.selectUser(userName,password) != null ) {
                 System.out.println("User Found!");
             } else {
                 System.out.println("User not found");
+                errorAlert.setTitle("Login Error");
+                errorAlert.setContentText("Error: Invalid Username / Password.");
+                errorAlert.showAndWait();
+                return;
             }
-
-
+            usernameTxtField.clear();
+            passwordTxtField.clear();
 
         }catch (Exception e) {
-
             System.out.println(e.getMessage());
-
         }
+    }
 
+    @FXML
+    void onActionExitProgram(ActionEvent event) {
+
+        ((Button) confirmationAlert.getDialogPane().lookupButton(ButtonType.OK)).setText("Exit");
+        confirmationAlert.setContentText("Are you sure you would like to exit the program?");
+        confirmationAlert.showAndWait();
+        if(confirmationAlert.getResult() == ButtonType.OK) {
+            System.exit(0);
+        } else return;
+    }
+
+    @FXML
+    void onActionClearFields(ActionEvent event) {
+
+        confirmationAlert.setContentText("Are you sure you want to clear the fields? Click OK to confirm.");
+        confirmationAlert.showAndWait();
+        if(confirmationAlert.getResult() == ButtonType.OK) {
+            usernameTxtField.clear();
+            passwordTxtField.clear();
+        } else return;
 
     }
+
+
 
 
 }
